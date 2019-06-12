20273da22e50dcace4ae52f8fd579c15bab8a393
SGS Venkatesh <venki.iitbbs@gmail.com>
2019-06-12 14:13:03 -0700

    Changes to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 pip show numpy
 pip install numpy==1.16.1
-!pip show numpy
-
-git clone https://github.com/shaoanlu/faceswap-GAN.git
-cd faceswap-GAN
+pip show numpy
 
 pip install moviepy
 pip install keras_vggface
